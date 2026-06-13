f442a4e8011d4ea36354e7f40082d7cb42cf82fe
Nazim6269 <nazimdev10022001@gmail.com>
2026-06-13 16:32:54 +0600

    fix: fix the button overflow

--- lib/presentation/widgets/outline_button.dart
+++ lib/presentation/widgets/outline_button.dart
@@ -23,41 +23,53 @@ class CustomOutlinedButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        width: double.infinity,
-        height: 56.h,
-        padding: EdgeInsets.all(14.h),
-        decoration: BoxDecoration(
-          color: fillColor ?? ColorManager.backgroundSecondary,
-          border: Border.all(
-            color: ColorManager.customOutlineButtonBorder,
-            width: 1.w,
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: onTap,
+        borderRadius: BorderRadius.circular(12.r),
+        child: Container(
+          width: double.infinity,
+          height: 56.h,
+          padding: EdgeInsets.symmetric(horizontal: 16.w),
+          decoration: BoxDecoration(
+            color: fillColor ?? ColorManager.backgroundSecondary,
+            border: Border.all(
+              color: color ?? ColorManager.customOutlineButtonBorder,
+              width: 1.w,
+            ),
+            borderRadius: BorderRadius.circular(12.r),
           ),
-          borderRadius: BorderRadius.circular(12.r),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (icon != null) ...[
-              SizedBox(
-                width: 20.w,
-                height: 20.h,
-                child: FittedBox(
-                  fit: BoxFit.contain,
-                  child: icon!,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (icon != null) ...[
+                SizedBox(
+                  width: 20.w,
+                  height: 20.h,
+                  child: FittedBox(fit: BoxFit.contain, child: icon!),
+                ),
+                SizedBox(width: 10.w),
+              ],
+              Flexible(
+                child: Text(
+                  title,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.center,
+                  style:
+                      textStyle ??
+                      getRegularStyle20_500(
+                        color: ColorManager.grayBlack400,
+                        fontSize: 18.sp,
+                      ),
                 ),
               ),
-              SizedBox(width: 10.w),
             ],
-            Text(
-              title,
-              style: textStyle ?? getRegularStyle20_500(color: ColorManager.grayBlack400,fontSize: 18.sp),
-            ),
-          ],
+          ),
         ),
       ),
     );
   }
 }
+
