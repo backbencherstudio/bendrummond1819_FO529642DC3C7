70aabb02a11d8f2b07beac903bb21497046f7fcc
Nazim6269 <nazimdev10022001@gmail.com>
2026-06-15 08:39:56 +0600

    fix: fix the api_endpoints

--- lib/core/network/api_endpoints.dart
+++ lib/core/network/api_endpoints.dart
@@ -1,30 +1,30 @@
 class ApiEndpoints {
   static const String baseUrl =
-      "https://officially-motel-savings-success.trycloudflare.com";
+      "https://theft-blues-holland-takes.trycloudflare.com/api";
   //===================== auth ApiEndpoints ===========================//
-  static const String register = 'api/auth/register';
-  static const String login = 'api/auth/login';
-  static const String switchRole = 'api/auth/switch-role';
-  static const String verifyMail = 'api/auth/verify-email';
-  static const String resendOtp = 'api/auth/resend-verification-email';
-  static const String loadUser = 'api/auth/me';
-  static const String updateProfile = 'api/auth/update';
-  static const String forgetPassword = 'api/auth/forgot-password';
-  static const String verifyResetOtp = 'api/auth/verify-email';
-  static const String resetPassword = 'api/auth/reset-password';
-  static const String logout = 'api/auth/logout';
-  static const String createAndagetJob = 'api/jobs';
+  static const String register = '/auth/register';
+  static const String login = '/auth/login';
+  static const String switchRole = '/auth/switch-role';
+  static const String verifyMail = '/auth/verify-email';
+  static const String resendOtp = '/auth/resend-verification-email';
+  static const String loadUser = '/auth/me';
+  static const String updateProfile = '/auth/update';
+  static const String forgetPassword = '/auth/forgot-password';
+  static const String verifyResetOtp = '/auth/verify-email';
+  static const String resetPassword = '/auth/reset-password';
+  static const String logout = '/auth/logout';
+  static const String createAndagetJob = '/jobs';
 
   //========================set-up ApiEndpoints=========================//
-  static const String setUp = 'api/set-up';
-  static const String addNewGoal = 'api/set-up/add-new-goal';
-  static const String addBill = 'api/set-up/add-bill';
-  static const String debts = 'api/set-up/debts';
-  static const String savingGoals = 'api/set-up/saving-goals';
-  static const String monthlyBills = 'api/set-up/monthly-bills';
-  static const String payIncomes = 'api/set-up/pay-incomes';
-  static String billById(int id) => 'api/set-up/bills/$id';
-  static String savingGoalById(int id) => 'api/set-up/saving-goals/$id';
-  static String billForPatch(int id) => 'api/set-up/bill/$id';
-  static String incomeForPatch(int id) => 'api/set-up/income/$id';
+  static const String setUp = '/set-up';
+  static const String addNewGoal = '/set-up/add-new-goal';
+  static const String addBill = '/set-up/add-bill';
+  static const String debts = '/set-up/debts';
+  static const String savingGoals = '/set-up/saving-goals';
+  static const String monthlyBills = '/set-up/monthly-bills';
+  static const String payIncomes = '/set-up/pay-incomes';
+  static String billById(int id) => '/set-up/bills/$id';
+  static String savingGoalById(int id) => '/set-up/saving-goals/$id';
+  static String billForPatch(int id) => '/set-up/bill/$id';
+  static String incomeForPatch(int id) => '/set-up/income/$id';
 }
